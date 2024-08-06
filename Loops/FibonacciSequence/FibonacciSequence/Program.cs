ef333022c96ed34ef305e868395d0da697b66e9e
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-06 19:21:10 +0400

    Fibonacci Sequence

---
+++ Loops/FibonacciSequence/FibonacciSequence/Program.cs
@@ -0,0 +1,40 @@
+﻿//Fibonacci Sequence Generator
+//Write a program that generates the first 50 numbers of the Fibonacci sequence.
+int fibonacciNumber = 0;
+int firstNum = 0;
+int secondNum = 1;
+int temp;
+while (fibonacciNumber != -1)
+{
+    Console.WriteLine ("Enter fibonacci sequence number or -1 for exit: ");
+    bool convert = Int32.TryParse(Console.ReadLine(), out fibonacciNumber);
+    if (fibonacciNumber == -1)
+        break;
+    else if (convert == false || fibonacciNumber < 0 )
+    {
+        Console.WriteLine("Incorrect data");
+        continue;
+    }
+    else if (fibonacciNumber >= 47)
+    {
+        Console.WriteLine("Maxium number is 47");
+        continue;
+    }
+    else
+    {
+        Console.Write("{0}, {1}, ", firstNum, secondNum);
+        for (int i = 2; i < fibonacciNumber; i++)
+        {
+            temp = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = temp;
+            if(fibonacciNumber - 1 == i)
+                Console.Write(" {0}", temp);
+            else
+                Console.Write(" {0},", temp);
+        }
+        Console.WriteLine();
+        firstNum = 0;
+        secondNum = 1;
+    }
+}
