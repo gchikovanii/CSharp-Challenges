dccae229cc4db91363ca04c56b54a40c9093ce91
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-07 12:07:32 +0400

    Prime Number

---
+++ Loops/PrimeNumbersFinder/PrimeNumbersFinder/Program.cs
@@ -0,0 +1,17 @@
+﻿//Prime Number Finder
+//Create a program that finds and prints all prime numbers between 1 and 10,000.
+
+for (int i = 2; i < 10000; i++)
+{
+    bool isPrime = true;
+    for (int j = 2; j * j <= i; j++)
+    {
+        if (i % j == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+        Console.Write("{0} ", i);
+}
