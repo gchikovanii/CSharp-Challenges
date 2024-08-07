443166970bf1693c8299f650c88586aae0093dca
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-07 12:52:01 +0400

    Matrix Multiplication

---
+++ Loops/MatrixMultiplication/MatrixMultiplication/Program.cs
@@ -0,0 +1,56 @@
+﻿//Matrix Multiplication
+//Implement matrix multiplication. Your program should take two matrices of size 3x3 and output their product.
+
+int length = 3;
+int[,] matrix = new int[3,3];
+for (int i = 0; i < length; i++)
+{
+    for (int j = 0; j < length; j ++)
+    {
+        Console.Write("Enter element at position {0} {1}: ",i,j);
+        int number = int.Parse(Console.ReadLine());
+        matrix[i,j] = number;
+    }
+}
+Console.WriteLine("Fill second matrix");
+int[,] matrixTwo = new int[3, 3];
+for (int i = 0; i < length; i++)
+{
+    for (int j = 0; j < length; j++)
+    {
+        Console.Write("Enter element at position {0} {1}: ", i, j);
+        int number = int.Parse(Console.ReadLine());
+        matrixTwo[i, j] = number;
+    }
+}
+
+Console.WriteLine("Multiplying 3x3 matrix on 3x3 matrix");
+Console.WriteLine("Calculating...");
+Thread.Sleep(1000);
+
+int[,] resultMatrix = new int[3, 3];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrixTwo.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            sum += matrix[i, k] * matrixTwo[k,j];
+        }
+        resultMatrix[i, j] = sum;
+    }
+    Console.WriteLine();
+}
+
+
+//Displaying result
+for (int i = 0; i < resultMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+        Console.Write(resultMatrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
--- Loops/PrimeNumbersFinder/PrimeNumbersFinder/Program.cs
+++ Loops/PrimeNumbersFinder/PrimeNumbersFinder/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Prime Number Finder
 //Create a program that finds and prints all prime numbers between 1 and 10,000.
 
-for (int i = 2; i < 10000; i++)
+for (int i = 30; i < 10000; i++)
 {
     bool isPrime = true;
     for (int j = 2; j * j <= i; j++)
