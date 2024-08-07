c1d973cad782afc45cb0282db2e30517860de1a1
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-07 10:39:09 +0400

    Time Complecity changed to O(n)

--- Loops/WeatherConsoleApp/WeatherConsoleApp/Program.cs
+++ Loops/WeatherConsoleApp/WeatherConsoleApp/Program.cs
@@ -13,38 +13,67 @@ for (int i = 0; i < days; i++)
 Console.WriteLine("Average temp is " + average(temperatures));
 Console.WriteLine("Min value is " + minValue(temperatures));
 Console.WriteLine("Max value is " + maxValue(temperatures));
-Console.WriteLine("Common " + mostCommon(weatherConditions));
+Console.WriteLine("Common " + commonInArray(weatherConditions));
 
 //Complecity Time is (O(n^2) 
-string mostCommon(string[] conditions)
+//string mostCommon(string[] conditions)
+//{
+//    int count = 0;
+//    string current = "";
+//    if (conditions == null || conditions.Length == 0)
+//        return string.Empty;
+//    for (int i = 0; i < conditions.Length; i++)
+//    {
+//        int tempCount = 1;
+//        for (int j = i + 1; j < conditions.Length; j++)
+//        {
+//            if (conditions[i] == conditions[j])
+//                tempCount++;
+//        }
+//        //Case if it's more than 50% return straight away
+//        if (tempCount > (conditions.Length / 2.0))
+//        {
+//            current = conditions[i];
+//            return current;
+//        }
+
+//        if (tempCount > count)
+//        {
+//            count = tempCount;
+//            current = conditions[i];
+//        }
+//    }
+//    return current;
+//}
+//Time Complecity O(n) 
+string commonInArray(string[] conditions)
 {
-    int count = 0;
-    string current = "";
     if (conditions == null || conditions.Length == 0)
         return string.Empty;
-    for (int i = 0; i < conditions.Length; i++)
-    {
-        int tempCount = 1;
-        for (int j = i + 1; j < conditions.Length; j++)
-        {
-            if (conditions[i] == conditions[j])
-                tempCount++;
-        }
-        //Case if it's more than 50% return straight away
-        if (tempCount > (conditions.Length / 2.0))
-        {
-            current = conditions[i];
-            return current;
-        }
 
-        if (tempCount > count)
+    Dictionary<string, int> keyValuePairs = new();
+    //Set keys and values. Key is condition name, int is count
+    foreach (var con in conditions)
+    {
+        if (keyValuePairs.ContainsKey(con))
+            keyValuePairs[con]++;
+        else
+            keyValuePairs[con] = 1;
+    }
+    int maxCount = 0;
+    string mostCommon = "";
+    //Find most common
+    foreach (var item in keyValuePairs)
+    {
+        if (item.Value > maxCount)
         {
-            count = tempCount;
-            current = conditions[i];
+            maxCount = item.Value;
+            mostCommon = item.Key;
         }
     }
-    return current;
+    return mostCommon;
 }
+
 double average(int[] temutures)
 {
     double sum = 0;
