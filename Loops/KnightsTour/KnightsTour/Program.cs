d4b410cad04ede12ac0bd7c39d4f09e5d6c0393e
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-08 21:03:11 +0400

    Total Count of moves

---
+++ Loops/KnightsTour/KnightsTour/Program.cs
@@ -0,0 +1,25 @@
+﻿//Knights Tour
+//Create a program to count the total number of valid knight moves on an 8x8 chessboard 
+
+int[,] board = new int[8, 8];
+int movesCounter = 0;
+
+int[] knightMovementsRows =
+{
+    2, 2, 1, 1, -1, -1, -2, -2
+};
+int[] knightMovementsCol = { 1, -1, 2, -2, -2, 2, -1, 1 };
+for (int row = 0; row < board.GetLength(0); row++)
+{
+    for (int col = 0; col < board.GetLength(1); col++)
+    {
+        for (int ki = 0; ki < knightMovementsRows.Length; ki++)
+        {
+            int moveX = knightMovementsRows[ki];
+            int moveY = knightMovementsCol[ki];
+            if (row + moveX >= 0 && row + moveX < 8 && col + moveY >= 0 && col + moveY < 8)
+                    movesCounter++;
+        }
+    }
+}
+Console.WriteLine("Total count is: {0}", movesCounter);
--- Loops/Permutations/Permutations/Program.cs
+++ Loops/Permutations/Permutations/Program.cs
@@ -1,25 +1,25 @@
-﻿//Write a program that generates all permutations using Backtracking algorithm
+﻿//Knights Tour
+//Create a program to solve the Knight's Tour problem on an 8x8 chessboard 
 
-string input = "ABC";
-List<string> permutations = new List<string>();
-GeneratePermutations("",input,permutations);
-foreach (var item in permutations)
+
+int[,] board = new int[8,8];
+int movesCounter = 0;
+
+int[] knightMovementsRows = 
 {
-    Console.WriteLine(item);
-}
-                                    
-void GeneratePermutations(string currentPermutation, string remainingCharacters, List<string> permutations)
+    2, 2, 1, 1, -1, -1, -2, -2 
+};
+int[] knightMovementsCol = {  1, -1, 2, -2, -2, 2, -1, 1 };
+for (int row = 0; row < board.GetLength(0); row++)
 {
-    if(remainingCharacters.Length == 0)
-        permutations.Add(currentPermutation);
-    else
+    for (int col = 0; col < board.GetLength(1); col++)
     {
-        for (int i = 0; i < remainingCharacters.Length; i++)
+        for (int ki = 0; ki < knightMovementsRows.Length; ki++)
         {
-            char chosenChar = remainingCharacters[i];
-
-            string newRemaining = remainingCharacters.Substring(0, i) + remainingCharacters.Substring(i + 1);
-            GeneratePermutations(currentPermutation + chosenChar, newRemaining, permutations);
+            if ((row + knightMovementsRows[ki] >= 0 && row + knightMovementsRows[ki] <= 8) && (col + knightMovementsCol[ki] >= 0 && col + knightMovementsCol[ki] <= 8))
+                movesCounter++;
         }
     }
 }
+Console.WriteLine("Total count is: {0}", movesCounter);
+
