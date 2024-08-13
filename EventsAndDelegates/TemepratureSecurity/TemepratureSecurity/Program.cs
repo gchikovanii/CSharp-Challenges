836650259b132a645d92ebb41fbe2035fff5fdcd
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-13 08:24:19 +0400

    event task implemented

---
+++ EventsAndDelegates/TemepratureSecurity/TemepratureSecurity/Program.cs
@@ -0,0 +1,13 @@
+﻿using TemepratureSecurity;
+
+TemperatureMonitor temperatureMonitor = new TemperatureMonitor();
+AlertControl alertControl = new AlertControl();
+Security security = new Security();
+
+temperatureMonitor.OnTemepratureChange += alertControl.Alert;
+temperatureMonitor.OnTemepratureChange += security.SecurityAction;
+Random random = new Random();
+temperatureMonitor.Treshold = 30;
+temperatureMonitor.Temperature = random.Next(20, 50);
+
+
---
+++ EventsAndDelegates/TemepratureSecurity/TemepratureSecurity/TemperatureMonitor.cs
@@ -0,0 +1,40 @@
+﻿namespace TemepratureSecurity
+{
+    public delegate void TemperatureChangeHandler(string message);
+
+    public class TemperatureMonitor
+    {
+        private int _temeprature;
+        public int Treshold { get; set; }
+        public int Temperature { get { return _temeprature; }
+            set 
+            {
+                _temeprature = value;
+                if (_temeprature > Treshold)
+                    OnEventRaised($"Temperature is {_temeprature}, it is more than treshold!");
+
+            } 
+        }
+        public event TemperatureChangeHandler OnTemepratureChange;   
+        public void OnEventRaised(string message)
+        {
+            OnTemepratureChange?.Invoke(message);
+        }
+    }
+    public class AlertControl
+    {
+        public void Alert(string message)
+        {
+            Console.WriteLine("Alert: " + message);
+        }
+    }
+
+    public class Security
+    {
+        public void SecurityAction(string message)
+        {
+            Console.WriteLine("We will cool the temeprature automatically!");
+        }
+    }
+
+}
