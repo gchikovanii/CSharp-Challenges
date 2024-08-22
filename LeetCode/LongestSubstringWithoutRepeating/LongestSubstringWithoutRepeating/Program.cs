46067602c6792eca7b72d1bc539e6870a3d363b7
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-22 13:54:31 +0400

    Length Of Longest Substring

---
+++ LeetCode/LongestSubstringWithoutRepeating/LongestSubstringWithoutRepeating/Program.cs
@@ -0,0 +1,29 @@
+﻿string input = "KukuKata";
+LengthOfLongestSubstring(input);
+static int LengthOfLongestSubstring(string s)
+{
+    List<char> charSet = new List<char>();
+    int start = 0;
+    int maxLength = 0;
+    string longestSubstring = "";
+
+    for (int end = 0; end < s.Length; end++)
+    {
+        while (charSet.Contains(s[end]))
+        {
+            charSet.Remove(s[start]);
+            start++;
+        }
+
+        charSet.Add(s[end]);
+        if (end - start + 1 > maxLength)
+        {
+            maxLength = end - start + 1;
+            longestSubstring = s.Substring(start, maxLength);
+        }
+    }
+
+    return longestSubstring.Length;
+}
+
+
