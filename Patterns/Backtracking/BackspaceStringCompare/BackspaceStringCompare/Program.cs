f61a49545d68a212dad4f8c3f613d8a7b474e23a
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-25 19:26:50 +0400

    Task done

---
+++ Patterns/Backtracking/BackspaceStringCompare/BackspaceStringCompare/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string s = "a#";
+string t = "b";
+Console.WriteLine(BackspaceCompare(s, t));
+static bool BackspaceCompare(string s, string t)
+{
+    string resultString(string k)
+    {
+        Stack<char> stack = new Stack<char>();
+        
+        foreach (var ks in k)
+        {
+            if(ks != '#')
+                stack.Push(ks);
+            else if (stack.Count > 0)
+                stack.Pop();
+        }
+        return new string(stack.ToArray());
+    }
+    return resultString(s).Equals(resultString(t));   
+}
+
