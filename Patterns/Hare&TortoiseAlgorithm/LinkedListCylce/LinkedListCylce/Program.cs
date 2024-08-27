3c973797e20e5c99bae44a55fb445b7b4dae6ef3
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-27 22:06:03 +0400

    Task Done

--- Patterns/Hare&TortoiseAlgorithm/LinkedListCylce/LinkedListCylce/Program.cs
+++ Patterns/Hare&TortoiseAlgorithm/LinkedListCylce/LinkedListCylce/Program.cs
@@ -1,1 +1,30 @@
 ﻿
+Console.WriteLine(  );
+
+static bool HasCycle(ListNode head)
+{
+    ListNode slow = head;
+    ListNode fast = head;
+    
+    while (fast != null && fast.next != null)
+    {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (fast == slow)
+            return true;
+    }
+    return false;
+}
+
+
+public class ListNode
+{
+     public int val;
+     public ListNode next;
+     public ListNode(int x)
+     {
+        val = x;
+        next = null;
+     }
+ }
+
