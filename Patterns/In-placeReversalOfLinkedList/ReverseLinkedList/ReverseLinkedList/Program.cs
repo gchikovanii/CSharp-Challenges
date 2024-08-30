c55d1db341a6f755f64bd131987dbaa5ef7661dc
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-30 22:00:03 +0400

    Linked List reversed IN O(N)

---
+++ Patterns/In-placeReversalOfLinkedList/ReverseLinkedList/ReverseLinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿static ListNode ReverseList(ListNode head)
+{
+    ListNode current = head;
+    ListNode previuos = null;
+    while(current != null)
+    {
+        var temp = current.next;
+        current.next = previuos;
+        previuos = current;
+        current = temp;
+    }
+    return previuos;
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0,ListNode next = null)
+    {
+        val = val;
+        next = null;
+    }
+}
