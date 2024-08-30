5b11ed33a4d942499fb46acb764f10e4babfc16f
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-30 06:50:52 +0400

    Singleton Design pattern

---
+++ DesignPatterns/Singleton/Singleton/Implementation.cs
@@ -0,0 +1,32 @@
+﻿namespace Singleton
+{
+    /// <summary>
+    ///  Singleton Patterns
+    /// </summary>
+    public class Logger
+    {
+        //private static Logger? _instance;
+
+        //Thread safe way
+        private static Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+        public static Logger Instance { get { return _lazyLogger.Value; } }
+        //public static Logger Instance 
+        //{
+        //    get
+        //    {
+        //        if( _instance == null )
+        //            _instance = new Logger();
+        //        return _instance;
+        //    }
+        //}
+
+        protected Logger()
+        {
+        }
+        public void Log(string message)
+        {
+            Console.WriteLine("Logging message to log: {0}",message);
+        }
+
+    }
+}
---
+++ DesignPatterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿using Singleton;
+
+//Logger logger = new Logger(); //impossible due to protection level
+var logger1 = Logger.Instance;
+var logger2 = Logger.Instance;
+
+if(logger1 == logger2 && logger2 == Logger.Instance)
+    Console.WriteLine("Instances are same");
+
+logger2.Log($"Message from {nameof(logger1)}");
+logger2.Log($"Message from {nameof(logger2)}");
