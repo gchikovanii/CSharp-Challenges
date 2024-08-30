37f6c1e08325e5faea5df78fcbea6a47f43b66eb
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-30 07:16:30 +0400

    loosly coupling via abstract classes and factoreies

---
+++ DesignPatterns/Factory/Factory/Implementation.cs
@@ -0,0 +1,89 @@
+﻿namespace Factory
+{
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryCode;
+        public CountryDiscountService(string code)
+        {
+            _countryCode = code;
+        }
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch (_countryCode)
+                {
+                    case "GE":
+                        return 50;
+                    default:
+                        return 10;
+                }
+            }
+        }
+
+    }
+
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _code;
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+        public override int DiscountPercentage
+        {
+            get
+            {
+                //logic here
+                return 15;
+            }
+        }
+
+    }
+
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryCode;
+
+        public CountryDiscountFactory(string countryCode)
+        {
+            _countryCode = countryCode;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryCode);
+        }
+    }
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
+
+
+}
---
+++ DesignPatterns/Factory/Factory/Program.cs
@@ -0,0 +1,8 @@
+﻿using Factory;
+
+var factories = new List<DiscountFactory> { new CodeDiscountFactory(Guid.NewGuid()), new CountryDiscountFactory("GE") };
+foreach (var factory in factories)
+{
+    var discountService = factory.CreateDiscountService();
+    Console.WriteLine($"Percentage: {discountService.DiscountPercentage} coming from {discountService}");
+}
