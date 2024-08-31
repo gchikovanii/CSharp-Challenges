2dd802c578f94d42e98d6382438bf1b801413b68
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-31 09:12:26 +0400

    Shallow Copy

---
+++ DesignPatterns/Prototype/Prototype/Implementation.cs
@@ -0,0 +1,42 @@
+﻿namespace Prototype
+{
+    public class Implementation
+    {
+        public abstract class Person
+        {
+            public abstract string Name { get; set; }
+            public abstract Person Clone();
+        }
+        public class Manager : Person
+        {
+
+            public override string Name { get ; set; }
+            public Manager(string name)
+            {
+                Name = name;
+            }
+            public override Person Clone()
+            {
+                //shallow copy
+                return (Person)MemberwiseClone();
+            }
+        }
+        public class Employee : Person
+        {
+
+            public override string Name { get; set; }
+            public Manager Manager { get; set; }
+            public Employee(string name, Manager manager)
+            {
+                Name = name;
+                Manager = manager;
+            }
+            public override Person Clone()
+            {
+                //shallow copy
+                return (Person)MemberwiseClone();
+            }
+        }
+
+    }
+}
---
+++ DesignPatterns/Prototype/Prototype/Program.cs
@@ -0,0 +1,13 @@
+﻿using static Prototype.Implementation;
+
+var manager = new Manager("Giorgi");
+var managerClone = (Manager)manager.Clone();
+Console.WriteLine($"Manager waas cloned: {managerClone.Name}");
+
+var emp = new Employee("Georginio", managerClone);
+var empClone = (Employee)emp.Clone();
+Console.WriteLine($"Employee waas cloned: {empClone.Name} with Manager {empClone.Manager.Name}");
+
+//change the manager name
+managerClone.Name = "Susana";
+Console.WriteLine($"Employee waas cloned: {empClone.Name} with Manager {empClone.Manager.Name}");
