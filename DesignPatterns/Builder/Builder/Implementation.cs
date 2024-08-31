87224f251972691849b5b88b0280ae4e305c7919
gchikovnaii25 <gchikovanii25@gmail.com>
2024-08-31 08:37:10 +0400

    Builder Pattern

---
+++ DesignPatterns/Builder/Builder/Implementation.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+
+namespace Builder
+{
+    public class Implementation
+    {
+
+        public class Car
+        {
+            private readonly List<string> _parts = new();
+            private readonly string _CarType;
+            public Car(string carType)
+            {
+                _CarType = carType;
+            }
+            public void AddCar(string part)
+            {
+                _parts.Add(part);
+            }
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var part in _parts)
+                {
+                    sb.Append($"Car of type {_CarType} has implemented a {part}.");
+                }
+                return sb.ToString();
+            }
+        }
+
+        public abstract class CarBuilder
+        {
+            public Car Car { get; private set; }
+            public CarBuilder(string carType)
+            {
+                Car = new Car(carType);
+            }
+            public abstract void BuildEngine();
+            public abstract void BuildFrame();
+        }
+
+        public class Audi : CarBuilder
+        {
+            public Audi() : base("Audi")
+            {
+            }
+            public override void BuildEngine()
+            {
+                Car.AddCar("Enginge v8");
+            }
+
+            public override void BuildFrame()
+            {
+                Car.AddCar("Red Carpet inside");
+            }
+        }
+        public class Chevrolet : CarBuilder
+        {
+            public Chevrolet() : base("Chevy")
+            {
+            }
+            public override void BuildEngine()
+            {
+                Car.AddCar("Enginge v15");
+            }
+
+            public override void BuildFrame()
+            {
+                Car.AddCar("White Ceramic Poly");
+            }
+        }
+
+
+        public class Garage
+        {
+            private CarBuilder? _carBuilder;
+            public Garage()
+            {
+            }
+            public void Construct(CarBuilder carBuilder) 
+            {
+                _carBuilder = carBuilder;
+                _carBuilder.BuildEngine();
+                _carBuilder.BuildFrame();
+
+            }
+            public void ShowCar()
+            {
+                Console.WriteLine(_carBuilder?.Car.ToString());
+            }
+        }
+
+    }
+}
---
+++ DesignPatterns/Builder/Builder/Program.cs
@@ -0,0 +1,9 @@
+﻿using static Builder.Implementation;
+
+Garage garage = new Garage();
+var audi = new Audi();
+var chevy = new Chevrolet();
+garage.Construct(audi);
+garage.ShowCar();
+garage.Construct(chevy);
+garage.ShowCar();
