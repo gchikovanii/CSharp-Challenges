efc1ff5d1f59f76e77ffc3678864aaf58b8c9810
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-01 07:23:13 +0400

    Adapter Done

---
+++ DesignPatterns/Structural/Adapter/Adapter/ObjectAdapterImplementation.cs
@@ -0,0 +1,53 @@
+﻿namespace Adapter
+{
+    public class ObjectAdapterImplementation
+    {
+        public class CityFromExtenralSystem
+        {
+            public CityFromExtenralSystem(string name, string nickName, int inhabitants)
+            {
+                Name = name;
+                NickName = nickName;
+                Inhabitants = inhabitants;
+            }
+            public string Name { get; private set; }
+            public string NickName { get; private set; }
+            public int Inhabitants { get; private set; }
+        }
+
+
+        public class ExternalSystem
+        {
+            public CityFromExtenralSystem GetCity()
+            {
+                return new CityFromExtenralSystem("Tbilisi", "T'bilisi", 1423520);
+            }
+        }
+
+        public class City
+        {
+            public City(string fullName, long inhabitants)
+            {
+                FullName = fullName;
+                Inhabitants = inhabitants;
+            }
+
+            public string FullName { get; private set; }
+            public long Inhabitants { get; private set; }
+        }
+
+        public interface ICityAdapter
+        {
+            City GetCity();
+        }
+        public class CityAdapter : ICityAdapter
+        {
+            public ExternalSystem ExternalSystem { get; private set; } = new();
+            public City GetCity()
+            {
+                var cityFromExternalSys = ExternalSystem.GetCity();
+                return new City($"{cityFromExternalSys.Name} - {cityFromExternalSys.NickName}", cityFromExternalSys.Inhabitants);
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Adapter/Adapter/Program.cs
@@ -0,0 +1,7 @@
+﻿//using static Adapter.ObjectAdapterImplementation;
+
+using static Adapter.ClassAdapterImplementation;
+
+ICityAdapter adapter = new CityAdapter();
+var city = adapter.GetCity();
+Console.WriteLine(city.FullName + " " + city.Inhabitants);
