745d92026976774ea23917e940d75cbe15ce0c1f
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-02 06:50:06 +0400

    Facade Done

---
+++ DesignPatterns/Facade/Facade/Implementation.cs
@@ -0,0 +1,53 @@
+﻿namespace Facade
+{
+    public class OrderService
+    {
+        public bool HasEnoughOrders(int customerId)
+        {
+            //Real calculation goes here
+            return customerId > 10;
+        }
+        
+
+    }
+    public class CustomerDiscoutBaseService
+    {
+        public double CalculateDiscountBase(int customerId)
+        {
+            //Real calculation here
+            return (customerId > 5) ? 20 : 10;
+        }
+    }
+   
+    public class DayOfWeekFactorService
+    {
+        public double CalculateByDayOfTheWeekFactor()
+        {
+            switch (DateTime.UtcNow.DayOfWeek)
+            {
+                case DayOfWeek.Saturday:
+                    return 0.2;
+                case DayOfWeek.Sunday:
+                    return 0.5;
+                default:
+                    return 1.2;
+            }
+        }
+    }
+
+    public class DiscountFacade
+    {
+        private readonly OrderService _orderService = new();
+        private readonly CustomerDiscoutBaseService _customerBaseService = new();
+        private readonly DayOfWeekFactorService _dayOfWeekService = new();
+
+        public double GetDiscount(int customerId)
+        {
+            if (!_orderService.HasEnoughOrders(customerId))
+                return 0;
+            return _customerBaseService.CalculateDiscountBase(customerId) * _dayOfWeekService.CalculateByDayOfTheWeekFactor();
+        }   
+
+    }
+
+}
---
+++ DesignPatterns/Facade/Facade/Program.cs
@@ -0,0 +1,5 @@
+﻿using Facade;
+
+var facade = new DiscountFacade();
+Console.WriteLine($"Discount percantage: {facade.GetDiscount(3)}");
+Console.WriteLine($"Discount percantage: {facade.GetDiscount(15)}");
