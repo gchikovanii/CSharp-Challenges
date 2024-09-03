4a27f620d79bf6400dec2abf4fdcefd70b84b872
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-03 06:37:03 +0400

    Virtual Proxy

---
+++ DesignPatterns/Proxy/Proxy/Implementation.cs
@@ -0,0 +1,54 @@
+﻿namespace Proxy
+{
+
+    public interface IDocument
+    {
+        void DisplayDocument();
+    }
+    public class Document : IDocument
+    {
+        public string? Title { get; private set; }
+        public string? Content { get; private set; }
+        public int AuthorId { get; private set; }
+        public DateTimeOffset LastAccesed { get; private set; }
+        private string _fileName;
+
+        public Document(string fileName)
+        {
+            _fileName = fileName;
+            LoadDocument(fileName);
+        }
+
+        private void LoadDocument(string fileName)
+        {
+            Console.WriteLine("Executing heavy action, loading data...");
+            Thread.Sleep(2000);
+            Title = "Test Title N1";
+            Content = "Test Content N1";
+            AuthorId = 1;
+            LastAccesed = DateTimeOffset.UtcNow;
+
+        }
+        public void DisplayDocument()
+        {
+            Console.WriteLine($"{Title} - {Content}");
+        }
+    }
+
+    public class DocumentProxy : IDocument
+    {
+        private string _fileName;
+        private Lazy<Document> _document;
+        public DocumentProxy(string fileName)
+        {
+            _fileName = fileName;
+            _document = new Lazy<Document>(() => new Document(_fileName));
+        }
+
+        public void DisplayDocument()
+        {
+            _document.Value.DisplayDocument();
+        }
+
+    }
+}
---
+++ DesignPatterns/Proxy/Proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Proxy;
+
+Console.WriteLine("Constructing document");
+var myDoc = new Document("TestDoc.pdf");
+Console.WriteLine("Documnet generated");
+myDoc.DisplayDocument();
+Console.WriteLine("-----------------------------------");
+Console.WriteLine("Document Proxy");
+var myDocProxy = new DocumentProxy("TestPtoxyDoc.pdf");
+Console.WriteLine("Documnet generated");
+myDocProxy.DisplayDocument();
