d4ef05df20969a4327f904986f9a52c9065c3ee0
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-03 06:58:38 +0400

    Flywieght implemented

---
+++ DesignPatterns/Flyweight/Flyweight/Imeplementation.cs
@@ -0,0 +1,58 @@
+﻿namespace Flyweight
+{
+    public interface ICharacter
+    {
+        void Draw(string fontFamily, int fontSize);
+    }
+
+    public class CharacterA : ICharacter
+    {
+        private char _actualChar = 'a';
+        private string _fontFamily = string.Empty;
+        private int _fontSize;
+        public void Draw(string fontFamily, int fontSize)
+        {
+            _fontFamily = fontFamily;
+            _fontSize = fontSize;
+            Console.WriteLine($"Drawing {_actualChar} -> {_fontFamily} - {fontSize}");
+        }
+    }
+    public class CharacterB : ICharacter
+    {
+        private char _actualChar = 'b';
+        private string _fontFamily = string.Empty;
+        private int _fontSize;
+        public void Draw(string fontFamily, int fontSize)
+        {
+            _fontFamily = fontFamily;
+            _fontSize = fontSize;
+            Console.WriteLine($"Drawing {_actualChar} -> {_fontFamily} - {fontSize}");
+        }
+    }
+
+    public class CharacterFactory
+    {
+        private readonly Dictionary<char, ICharacter> _characters = new();
+        public ICharacter? GetCharacter(char character)
+        {
+            if (_characters.ContainsKey(character))
+            {
+                Console.WriteLine("Character reuse");
+                return _characters[character];
+            }
+            switch (character)
+            {
+                case 'a':
+                    _characters[character] = new CharacterA();
+                    return _characters[character];
+                case 'b':
+                    _characters[character] = new CharacterB();
+                    return _characters[character];
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+
+}
---
+++ DesignPatterns/Flyweight/Flyweight/Program.cs
@@ -0,0 +1,16 @@
+﻿using Flyweight;
+
+var characters = "abba";
+var characterFacotyr = new CharacterFactory();
+
+var charObj = characterFacotyr.GetCharacter(characters[0]);
+charObj?.Draw("Sant-serif", 16);
+
+charObj = characterFacotyr.GetCharacter(characters[1]);
+charObj?.Draw("Arial", 18);
+
+charObj = characterFacotyr.GetCharacter(characters[2]);
+charObj?.Draw("Google Fonts Default", 14);
+
+charObj = characterFacotyr.GetCharacter(characters[3]);
+charObj?.Draw("Mememento", 12);
