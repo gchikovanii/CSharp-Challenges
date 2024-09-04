2ded8d635a82a2720eebfa892f707d648aaa9203
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-04 06:41:45 +0400

    Strategy pattern done

---
+++ DesignPatterns/Behavioral/Strategy/Strategy/Implementation.cs
@@ -0,0 +1,50 @@
+﻿namespace Strategy
+{
+    public class Order
+    {
+        public Order(int amount, string customer, string name)
+        {
+            Amount = amount;
+            Customer = customer;
+            Name = name;
+        }
+
+        public int Amount { get; set; }
+        public string Customer { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public IExportService? ExportService { get; set; }
+        public void Export()
+        {
+            ExportService?.Export(this);
+        }
+    }
+    public interface IExportService
+    {
+        void Export(Order order);
+    }
+    public class JsonExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to JSON");
+        }
+    }
+    public class XmlExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to XML");
+        }
+    }
+    public class CsvExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"Exporting {order.Name} to CSV");
+        }
+    }
+
+
+}
---
+++ DesignPatterns/Behavioral/Strategy/Strategy/Program.cs
@@ -0,0 +1,9 @@
+﻿using Strategy;
+
+var order = new Order(20, "Bachuki", "Tesla model 2 for Geeks");
+order.ExportService = new CsvExportService();
+order.Export();
+
+order.ExportService = new JsonExportService();
+order.Export();
+
