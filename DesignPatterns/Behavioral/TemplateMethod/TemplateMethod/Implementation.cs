95383e0e8f849a61467508732ae7983f075291e5
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-04 06:31:06 +0400

    Temp method pattern done

---
+++ DesignPatterns/Behavioral/TemplateMethod/TemplateMethod/Implementation.cs
@@ -0,0 +1,54 @@
+﻿namespace TemplateMethod
+{
+    public abstract class MailParser
+    {
+        public virtual void FindServer()
+        {
+            Console.WriteLine("Finding Server");
+        }
+        public abstract void AuthenticateToServer();
+
+        public string ParseHtmlBody(string identifier)
+        {
+            Console.WriteLine("Parsing html body...");
+            return $"This is body of Identifier: {identifier}";
+        }
+
+        public string ParseMailBody(string identifier)
+        {
+            Console.WriteLine("Parsing mail body in template method...");
+            FindServer();
+            AuthenticateToServer();
+            return ParseHtmlBody(identifier);
+        }
+
+    }
+
+    public class ExchangeMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to exchange");
+        }
+    }
+    public class ApacheMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Apache");
+        }
+    }
+
+    public class EudoraMailParser : MailParser
+    {
+        public override void AuthenticateToServer()
+        {
+            Console.WriteLine("Connecting to Eudora");
+        }
+        public override void FindServer()
+        {
+            Console.WriteLine("Finding eudora server");
+        }
+    }
+
+}
---
+++ DesignPatterns/Behavioral/TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using TemplateMethod;
+
+ExchangeMailParser exMailParser = new();
+Console.WriteLine(exMailParser.ParseMailBody("asdskmsadldkfasdf11111"));
+Console.WriteLine();
+
+ApacheMailParser apMailParser = new();
+Console.WriteLine(apMailParser.ParseMailBody("asdskmsadldkfasdf22222"));
+Console.WriteLine();
+
+EudoraMailParser edMailParser = new();
+Console.WriteLine(edMailParser.ParseMailBody("asdskmsadldkfasdf33333"));
+Console.WriteLine();
