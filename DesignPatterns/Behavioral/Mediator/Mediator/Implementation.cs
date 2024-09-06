efbbe6e6510add70bf2e8895f5cdf3fe7c515032
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-06 06:43:57 +0400

    Task is done

---
+++ DesignPatterns/Behavioral/Mediator/Mediator/Implementation.cs
@@ -0,0 +1,99 @@
+﻿namespace Mediator
+{
+    public interface IChatRoom
+    {
+        void Register(TeamMember member);
+        void Send(string from, string message);
+        void Send(string from, string to, string message);
+        void SendTo<T> (string from,  string message) where T : TeamMember;
+    }
+
+    public abstract class TeamMember
+    {
+        private IChatRoom? _room;
+
+        public string Name { get; set; }
+        protected TeamMember(string name)
+        {
+            Name = name;
+        }
+        internal void SetChatRoom(IChatRoom room)
+        {
+            _room = room;
+        }
+        public void Send(string message)
+        {
+            _room?.Send(Name,message);  
+        }
+        public void Send(string to, string message)
+        {
+            _room?.Send(Name,to,message);
+        }
+        public void SendTo<T>(string message) where T: TeamMember 
+        {
+            _room?.SendTo<T>(Name, message);
+        }
+        public virtual void Receive(string from, string message)
+        {
+            Console.WriteLine($"Message {from} to {Name} with message: {message}");
+        }
+       
+    }
+
+
+    public class AccountManager : TeamMember
+    {
+        public AccountManager(string name) : base(name)
+        {
+        }
+        public override void Receive(string from, string message)
+        {
+            Console.WriteLine($"{nameof(AccountManager)} {Name} received: ");
+            base.Receive(from, message);
+        }
+    }
+    public class Lawyer : TeamMember
+    {
+        public Lawyer(string name) : base(name)
+        {
+        }
+        public override void Receive(string from, string message)
+        {
+            Console.WriteLine($"{nameof(Lawyer)} {Name} received: ");
+            base.Receive(from, message);
+        }
+    }
+    public class TeamChatRoom : IChatRoom
+    {
+        private readonly Dictionary<string, TeamMember> teamMembers = new();
+        public void Register(TeamMember member)
+        {
+            member.SetChatRoom(this);
+            if(!teamMembers.ContainsKey(member.Name))
+                teamMembers.Add(member.Name, member);
+        }
+
+        public void Send(string from, string message)
+        {
+            foreach (var mes in teamMembers.Values)
+            {
+                mes.Receive(from,message);
+            }
+        }
+
+        public void Send(string from, string to, string message)
+        {
+            var teamMember = teamMembers[to];
+            teamMember.Receive(from, message);
+        }
+
+        public void SendTo<T>(string from, string message) where T : TeamMember
+        {
+            foreach (var teamMember in teamMembers.Values.OfType<T>())
+            {
+                teamMember.Send(from, message);
+            }
+        }
+    }
+
+}
---
+++ DesignPatterns/Behavioral/Mediator/Mediator/Program.cs
@@ -0,0 +1,19 @@
+﻿using Mediator;
+
+TeamChatRoom teamChatRoom = new();
+
+var gio = new Lawyer("Giorgi");
+var luka = new Lawyer("Luka");
+var oto = new Lawyer("Oto");
+var beka = new Lawyer("Beka");
+
+teamChatRoom.Register(gio);
+teamChatRoom.Register(luka);
+teamChatRoom.Register(oto);
+teamChatRoom.Register(beka);
+gio.Send("Giorgiii says hi");
+luka.Send("Hello giys from luka");
+oto.Send("Giorgi", "Hallo");
+
+Console.WriteLine("---------------------");
+beka.SendTo<Lawyer>("Task is done!!!");
