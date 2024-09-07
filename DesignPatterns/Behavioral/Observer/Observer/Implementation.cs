4fad149d78cf3d3d56247070450a7583f92793ae
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-07 06:44:22 +0400

    Task is done

---
+++ DesignPatterns/Behavioral/Observer/Observer/Implementation.cs
@@ -0,0 +1,69 @@
+﻿namespace Observer
+{
+    public class TicketChange
+    {
+        public TicketChange(int amount, int artistId)
+        {
+            Amount = amount;
+            ArtistId = artistId;
+        }
+
+        public int Amount { get; set; }
+        public int ArtistId { get; set; }
+    }
+    public abstract class TicketChangeNotifier
+    {
+        private List<ITicketChangeListener> _observers = new();
+        public void AddObserver(ITicketChangeListener listener) 
+        {
+            _observers.Add(listener);
+        }
+        public void RemoveObserver(ITicketChangeListener listener)
+        {
+            _observers.Remove(listener);
+        }
+
+        public void Notify(TicketChange ticketChange)
+        {
+            foreach (var obs in _observers)
+            {
+                obs.ReceiveTicketChangeNotification(ticketChange);
+            }
+        }
+
+    }
+
+    public interface ITicketChangeListener
+    {
+        void ReceiveTicketChangeNotification(TicketChange ticketChange);
+    }
+
+
+    public class OrderService : TicketChangeNotifier
+    {
+        public void CompleteTicektSale(int artistId, int amount)
+        {
+            //working with Database
+            Console.WriteLine($"{nameof(OrderService)} is changing its state");
+            // notify observer
+            Console.WriteLine($"{nameof(OrderService)} notifying observer");
+            Notify(new TicketChange(artistId, amount));
+        }
+    }
+
+    public class TicketResellerService : ITicketChangeListener
+    {
+        public void ReceiveTicketChangeNotification(TicketChange ticketChange)
+        {
+            Console.WriteLine($"{nameof(TicketResellerService)} notified of ticket change: artist {ticketChange.ArtistId} and amount: {ticketChange.Amount}");
+        }
+    }
+    public class TicketStockService : ITicketChangeListener
+    {
+        public void ReceiveTicketChangeNotification(TicketChange ticketChange)
+        {
+            Console.WriteLine($"{nameof(TicketStockService)} notified of ticket change: artist {ticketChange.ArtistId} and amount: {ticketChange.Amount}");
+        }
+    }
+
+}
---
+++ DesignPatterns/Behavioral/Observer/Observer/Program.cs
@@ -0,0 +1,13 @@
+﻿using Observer;
+
+TicketStockService ticketStockService = new ();
+TicketResellerService ticketResellerService = new ();
+OrderService orderService = new ();
+
+orderService.AddObserver(ticketStockService);
+orderService.AddObserver(ticketResellerService);
+
+orderService.CompleteTicektSale(1, 4);
+Console.WriteLine("-----------------------");
+orderService.RemoveObserver(ticketResellerService);
+orderService.CompleteTicektSale(2, 5);
