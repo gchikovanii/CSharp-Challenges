3a1b4c30f83f89c2bbc105276b7a3fe614bd90aa
gchikovnaii25 <gchikovanii25@gmail.com>
2024-09-08 06:49:59 +0400

    Task Done

---
+++ DesignPatterns/Behavioral/Iterator/Iterator/Implementation.cs
@@ -0,0 +1,69 @@
+﻿namespace Iterator
+{
+    public class Person
+    {
+        public Person(string name, string country)
+        {
+            Name = name;
+            Country = country;
+        }
+
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+
+    public interface IPeopleCollection
+    {
+        IPeoplIterator CreaateIterator();
+    }
+
+    public class PeopleCollection : List<Person>, IPeopleCollection
+    {
+        public IPeoplIterator CreaateIterator()
+        {
+            return new PeopleIterator(this);
+        }
+    }
+    public class PeopleIterator : IPeoplIterator
+    {
+        private PeopleCollection _peopleCollection;
+        private int _current;
+        public PeopleIterator(PeopleCollection peopleCollection)
+        {
+            _peopleCollection = peopleCollection;
+        }
+
+        public Person CurrentItem
+        {
+            get { return _peopleCollection.OrderBy(i => i.Name).ToList()[_current]; }
+        }
+
+        public bool IsDone
+        {
+            get { return _current >= _peopleCollection.Count; }
+        }
+
+        public Person First()
+        {
+            _current = 0;
+            return _peopleCollection.OrderBy(i => i.Name).ToList()[_current];
+        }
+
+        public Person Next()
+        {
+            _current++;
+            if (!IsDone)
+                return _peopleCollection.OrderBy(i => i.Name).ToList()[_current];
+            else
+                return null;
+        }
+    }
+    public interface IPeoplIterator
+    {
+        Person First();
+        Person Next();
+        Person CurrentItem { get; }
+        bool IsDone { get; }
+    }
+
+}
---
+++ DesignPatterns/Behavioral/Iterator/Iterator/Program.cs
@@ -0,0 +1,15 @@
+﻿using Iterator;
+
+PeopleCollection people = new();
+
+people.Add(new Person("Alan Pizz", "Fra"));
+people.Add(new Person("Besarion Gabashvili", "Geo"));
+people.Add(new Person("Ed Mylet", "Eng"));
+people.Add(new Person("Jordan Peterson", "Can"));
+
+var peopleIterator = people.CreaateIterator();
+
+for (Person person = peopleIterator.First(); !peopleIterator.IsDone; person = peopleIterator.Next())
+{
+    Console.WriteLine(person.Name);
+}
